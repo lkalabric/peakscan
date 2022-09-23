9d6cfbbf34f9bfc22758154588a7a0f56f0dfd61
Luciano Kalabric <91573364+lkalabric@users.noreply.github.com>
2022-09-23 18:19:27 -0500

    Create storing_inds.R

---
+++ storing_inds.R
@@ -0,0 +1,3 @@
+storing_inds <- function (folder, channels = NULL, fourier = TRUE, saturated = TRUE, 
+                                lets.pullup = FALSE, plotting = FALSE, 
+                                rawPlot = FALSE, llength = 3000, ulength = 80000) 
